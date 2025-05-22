dc65a6b9a852444b4f8bdd6b58b08ba33e55c74a
Sunilobi <106995530+Sunilobi@users.noreply.github.com>
2025-05-23 01:36:40 +0530

    Update compile_sketch.bat

--- scripts/compile_sketch.bat
+++ scripts/compile_sketch.bat
@@ -1,7 +1,14 @@
+@echo off
+setlocal
+
 set ARDUINO_DATA=%ARDUINO_DATA%
 set ARDUINO_USER_DIR=%ARDUINO_USER_DIR%
+
 mkdir "%BUILD_DIR%" 2>nul || echo Build folder exists
+
 %ARDUINO_CLI% compile ^
     --fqbn arduino:avr:nano:cpu=atmega328old ^
     --build-path "%BUILD_DIR%" ^
     sketches/hil_demo
+
+endlocal
